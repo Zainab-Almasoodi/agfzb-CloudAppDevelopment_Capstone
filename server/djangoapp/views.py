6cde249bfbb32327ce2330787d2d51c13bb64e8f
Zainab Al-Masoodi <zainab_almasoodi@yahoo.com>
2023-10-06 15:48:43 -0400

    Adding contact us page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -24,10 +24,12 @@ def about(request):
     if request.method == 'GET':
         return render(request, 'djangoapp/about.html', context)
 
-
-
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
