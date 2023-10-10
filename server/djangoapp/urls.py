679ab05fea0e38b1b412371dafaecd56255130b6
Zainab Al-Masoodi <zainab_almasoodi@yahoo.com>
2023-10-10 12:37:18 -0400

    added login logout registaration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,8 +17,9 @@ urlpatterns = [
     path(route='registration', view=views.registration_request, name='registration'),
 
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
     path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
